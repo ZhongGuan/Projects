dc2c4f62b8edfab29761dcd388387ebe8b4a4210
guanzh <guanzh.email@gmail.com>
2014-08-24 01:03:51 +0800

    1

--- JavaStudy/src/test/Main.java
+++ JavaStudy/src/test/Main.java
@@ -7,7 +7,7 @@ public class Main {
 		System.out.println("test");
 		System.out.println("test");
 		System.out.println("test");
-		System.out.println("test111");
+		System.out.println("test111222");
 		
 	}
 
